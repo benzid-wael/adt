5c23dc5eb876bf08450395b69a8c698d109540ec
Wael Ben Zid <benzid.wael@hotmail.fr>
2020-11-25 11:23:59 +0400

    More tests added

--- tests/trees/test_segment_tree.py
+++ tests/trees/test_segment_tree.py
@@ -18,3 +18,38 @@ def test_init_creates_segment_tree_from_given_array(array, kind, expected):
     actual = SegmentTreeFactory.create(array, kind)
     # then
     assert actual.tree == expected
+
+
+@pytest.mark.parametrize('array,kind,start,end,expected', [
+    ([1, 2], 'sum', 0, 0, 1),
+    ([1, 2], 'sum', 1, 1, 2),
+    ([1, 2], 'sum', 0, 1, 3),
+    ([1, 2], 'min', 0, 1, 1),
+    ([1, 2], 'max', 0, 1, 2),
+    ([4, 8, 1, 10, 2, 5, 13, 9], 'sum', 0, 2, 13),
+])
+def test_query_static_arrays(array, kind, start, end, expected):
+    # given
+    testee = SegmentTreeFactory.create(array, kind)
+    # when
+    actual = testee.query(start, end)
+    # then
+    assert actual == expected
+
+
+@pytest.mark.parametrize('array,kind,start,end', [
+    ([1, 2], 'sum', 0, 0),
+    ([1, 2], 'sum', 1, 1),
+    ([1, 2], 'sum', 0, 1),
+    ([4, 8, 1, 10, 2, 5, 13, 9], 'sum', 0, 2),
+])
+def test_range_sum_query_for_dynamic_arrays(array, kind, start, end):
+    # given
+    testee = SegmentTreeFactory.create(array, kind)
+    testee.update(start, 0)
+    before = testee.query(start, end)
+    # when
+    testee.update(start, 5)
+    # then
+    actual = testee.query(start, end)
+    assert actual == before + 5
